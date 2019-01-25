580e1984f83568eb875fe3c9a1cef69746079305
sdaaw <ma.oskari@gmail.com>
2019-01-25 22:36:36 +0200

    added .sln

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -6,16 +6,4 @@ public class SceneManager : MonoBehaviour
 {
 
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/SceneState.cs
+++ Assets/Scripts/SceneState.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class SceneState : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public List<GameObject> objects = new List<GameObject>();
+    public GameObject character;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
+
+
 }
